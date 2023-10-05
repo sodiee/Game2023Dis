898d811f810b374a4d5c17269844ece0ee1f48c1
lucasholm <luaheaa@students.eaaa.dk>
2023-10-05 11:59:49 +0200

    2 Klasser med server og serverThread

---
+++ Skeleton/src/Game2023/ServerThread.java
@@ -0,0 +1,31 @@
+package Game2023;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ServerThread {
+
+    Socket connsocket;
+
+    public ServerThread(Socket connSocket) {
+        this.connsocket = connSocket;
+    }
+
+    public void run() {
+        try {
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connsocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(connsocket.getOutputStream());
+            while(true) {
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
